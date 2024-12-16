afe14ae9dda04e94396f3c3c0b0d8b026d70c2c9
Philipp Winkler <mail@philslab.ninja>
2024-12-16 09:11:31 +0100

    exclude Gitlab
    Signed-off-by: Philipp Winkler <mail@philslab.ninja>

--- server/events/models/models.go
+++ server/events/models/models.go
@@ -96,7 +96,7 @@ func NewRepo(vcsHostType VCSHostType, repoFullName string, cloneURL string, vcsU
 	// and because the caller in that case actually constructs the clone url
 	// from the repo name and so there's no point checking if they match.
 	// Azure DevOps also does not require .git at the end of clone urls.
-	if vcsHostType != BitbucketServer && vcsHostType != AzureDevops {
+	if vcsHostType != BitbucketServer && vcsHostType != AzureDevops && vcsHostType != Gitlab {
 		expClonePath := fmt.Sprintf("/%s.git", repoFullName)
 		if expClonePath != cloneURLParsed.Path {
 			return Repo{}, fmt.Errorf("expected clone url to have path %q but had %q", expClonePath, cloneURLParsed.Path)
